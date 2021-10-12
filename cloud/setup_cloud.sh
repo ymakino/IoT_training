cc33d1d640d729176b6c385f78711b358fb54bd6
Yoshiki Makino <ymakino@users.noreply.github.com>
2021-10-12 21:40:43 +0900

    Update setup_cloud.sh

--- cloud/setup_cloud.sh
+++ cloud/setup_cloud.sh
@@ -8,7 +8,7 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update
 sudo apt-get -y install docker-ce
 sudo usermod -aG docker $USER
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 sudo apt-get -y install git
@@ -16,7 +16,7 @@ sudo apt-get -y install git
 cd ~/Documents
 git clone https://github.com/ymakino/IoT_training.git
 cp -ri ~/Documents/IoT_training/materials/efk ~/Desktop
-mv ~/Documents/IoT_training /tmp
+mv ~/Documents/IoT_training `mktemp -d`
 
 cat <<EOF > ~/Desktop/Kibana.desktop
 [Desktop Entry]
@@ -29,3 +29,8 @@ EOF
 
 cd ~/Desktop/efk
 sg docker "docker-compose create"
+
+convert +antialias -font DejaVu-Sans -fill "#FFFFFF" -gravity SouthEast -pointsize 256 -annotate +128+64 Cloud /usr/share/backgrounds/warty-final-ubuntu.png ~/Pictures/ubg-cloud.png
+
+echo gsettings set org.gnome.desktop.background picture-uri "file://`realpath ~/Pictures/ubg-cloud.png`"
+gsettings set org.gnome.desktop.background picture-uri "file://`realpath ~/Pictures/ubg-cloud.png`"
