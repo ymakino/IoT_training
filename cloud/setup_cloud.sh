ca89062577d281092107a9b67d52f6a5842055ed
Yoshiki Makino <ymakino@users.noreply.github.com>
2021-10-22 17:45:13 +0900

    Update setup_cloud.sh

--- cloud/setup_cloud.sh
+++ cloud/setup_cloud.sh
@@ -11,7 +11,7 @@ sudo usermod -aG docker $USER
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-sudo apt-get -y install git
+sudo apt-get -y install git mosquitto-clients
 
 cd ~/Documents
 git clone https://github.com/ymakino/IoT_training.git
