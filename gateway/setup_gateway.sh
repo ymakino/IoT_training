4e69b6b616f45776f2796fb47115e49a2b4687b6
Yoshiki Makino <ymakino@users.noreply.github.com>
2022-05-09 17:28:52 +0900

    Update setup_gateway.sh

--- gateway/setup_gateway.sh
+++ gateway/setup_gateway.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 AREANET_IF=eno1
-NETBEANS_VERSION=12.5
+NETBEANS_VERSION=13
 
 # Install packages
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y install default-jdk maven git
+sudo apt -y install wireshark mosquitto-clients zenmap
+sudo usermod -aG wireshark $USER
 
 # Install NetBeans
 cd /tmp
-wget -c https://ftp.jaist.ac.jp/pub/apache/netbeans/netbeans/${NETBEANS_VERSION}/Apache-NetBeans-${NETBEANS_VERSION}-bin-linux-x64.sh 
+wget -c https://ftp.jaist.ac.jp/pub/apache/netbeans/netbeans-installers/${NETBEANS_VERSION}/Apache-NetBeans-${NETBEANS_VERSION}-bin-linux-x64.sh 
 echo sh Apache-NetBeans-${NETBEANS_VERSION}-bin-linux-x64.sh  --silent
 sh Apache-NetBeans-${NETBEANS_VERSION}-bin-linux-x64.sh  --silent
 sed -i -e 's|^netbeans_jdkhome=.*$|netbeans_jdkhome="/usr/lib/jvm/default-java"|' ~/netbeans-${NETBEANS_VERSION}/netbeans/etc/netbeans.conf
@@ -27,20 +29,19 @@ git clone https://github.com/ymakino/humming.git
 cd ~/NetBeansProjects/echoMQTT/lib && JAVA_HOME=/usr/lib/jvm/default-java sh ./build_paho.sh
 
 # Open NetBeans
+echo
+echo Open echowand and echoMQTT projects
+echo Build echoMQTT
+echo Then quit NetBeans
 ~/netbeans-${NETBEANS_VERSION}/netbeans/bin/netbeans
 
-# Add echowand and echoMQTT projects here
-# Build echoMQTT here
-
 # Prepare echoMQTT
-
 cd ~/Documents
 git clone https://github.com/ymakino/IoT_training.git
 cp ~/Documents/IoT_training/materials/echoMQTT/rules.xml ~/Desktop
 mv ~/Documents/IoT_training `mktemp -d`
 
 # Prepare echoMQTT
-
 cd ~/Desktop
 cat <<EOF > run.sh
 #!/bin/sh
@@ -50,13 +51,12 @@ EOF
 
 chmod +x run.sh
 
-sudo apt -y install wireshark mosquitto-clients zenmap
-sudo usermod -aG wireshark $USER
-
+# Add ECHONET Lite dissector
 mkdir -p ~/.config/wireshark
 cd ~/.config/wireshark
 git clone https://github.com/mzyy94/ECHONET-Lite-dissector plugins
 
+# Setup the background picture
 convert +antialias -font DejaVu-Sans -fill "#FFFFFF" -gravity SouthEast -pointsize 256 -annotate +128+64 Gateway /usr/share/backgrounds/warty-final-ubuntu.png ~/Pictures/ubg-gateway.png
 
 echo gsettings set org.gnome.desktop.background picture-uri "file://`realpath ~/Pictures/ubg-gateway.png`"
