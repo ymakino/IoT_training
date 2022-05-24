b7c55e598b5a101a31083f398fe14b50b790ffd7
Yoshiki Makino <ymakino@users.noreply.github.com>
2022-05-24 17:45:06 +0900

    Update setup_device.sh

--- device/setup_device.sh
+++ device/setup_device.sh
@@ -13,7 +13,7 @@ cd ~/Documents
 git clone https://github.com/ymakino/IoT_training.git
 cp -r ~/Documents/IoT_training/materials/humming/VirtualAC ~/Desktop/
 cp -r ~/Documents/IoT_training/materials/humming/scripts ~/Desktop/
-chmod +x ~/Desktop/run.sh ~/Desktop/scripts/*.sh ~/Desktop/scripts/*.py
+chmod +x ~/Desktop/run.sh ~/Desktop/scripts/*.sh ~/Desktop/scripts/*.py ~/Desktop/scripts/*/*.sh
 mv ~/Documents/IoT_training `mktemp -d`
 
 cat <<EOF > ~/Desktop/run.sh
@@ -23,4 +23,12 @@ cd ${HOME}/Desktop/scripts
 java -cp ${HOME}/Desktop/VirtualAC/VirtualAC.jar:${HOME}/Desktop/VirtualAC/lib/humming.jar humming.Humming -i ${ETH_IF}  bme280.xml lighting.xml VirtualAC.xml
 EOF
 
+cat <<EOF > ~/Desktop/run_with_dummy_sensor.sh
+#!/bin/sh
+cd ${HOME}/Desktop/scripts
+
+java -cp ${HOME}/Desktop/VirtualAC/VirtualAC.jar:${HOME}/Desktop/VirtualAC/lib/humming.jar humming.Humming -i ${ETH_IF}  dummy_sensor/sensor.xml lighting.xml VirtualAC.xml
+EOF
+
 chmod +x ~/Desktop/run.sh
+chmod +x ~/Desktop/run_with_dummy_sensor.sh
